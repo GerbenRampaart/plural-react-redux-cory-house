c502f33a8f79b2fd546973118085d99589b43f82
Gerben Rampaart <Gerben Rampaart>
2017-02-06 08:45:02 +0100

    Starting 'Create Form Change Handler'

--- src/components/course/CourseForm.js
+++ src/components/course/CourseForm.js
@@ -17,6 +17,43 @@ const CourseForm = ({course, allAuthors, onSave, onChange, loading, errors}) => 
                 name="authorId"
                 label="Author"
                 value={course.authorId}
+                defaultOption="Select Author"
+                options={allAuthors}
+                onChange={onChange}
+                error={errors.authorId}/>
+
+            <TextInput
+                name="category"
+                label="Category"
+                value={course.category}
+                onChange={onChange}
+                error={errors.category}/>
+
+            <TextInput
+                name="length"
+                label="Length"
+                value={course.length}
+                onChange={onChange}
+                error={errors.length}/>
+
+            <input
+                type="submit"
+                disabled={loading}
+                value={loading ? 'Saving...' : 'Save'}
+                className="btn btn-primary"
+                onClick={onSave}/>
+
         </form>
     );
 }
+
+CourseForm.propTypes = {
+    course: React.PropTypes.object.isRequired,
+    allAuthors: React.PropTypes.array,
+    onSave: React.PropTypes.func.isRequired,
+    onChange: React.PropTypes.func.isRequired,
+    loading: React.PropTypes.bool,
+    errors: React.PropTypes.object
+};
+
+export default CourseForm;
